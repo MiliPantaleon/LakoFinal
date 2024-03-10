7217cd752508bcab1724f823aacd5fd6abf17efa
MiliPantaleon <mmpantaleon@gmail.com>
2024-03-10 13:45:47 -0300

    git add 00

---
+++ enviar_formulario.php
@@ -0,0 +1,30 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = htmlspecialchars($_POST["full-name"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $phone = htmlspecialchars($_POST["phone"]);
+        $company = htmlspecialchars($_POST["company"]);
+        $job_title = htmlspecialchars($_POST["job-title"]);
+        $date = htmlspecialchars($_POST["date"]);
+        $message = htmlspecialchars($_POST["message"]);
+
+        $to = "ventas@lako.com.ar";
+        $subject = "Nuevo contacto de la página web";
+        $headers = "From: $email" . "\r\n" .
+                   "Reply-To: $email" . "\r\n" .
+                   "X-Mailer: PHP/" . phpversion();
+        $body = "Nombre completo: $name\n" .
+                "Email: $email\n" .
+                "Teléfono: $phone\n" .
+                "Empresa: $company\n" .
+                "Cargo: $job_title\n" .
+                "Fecha de nacimiento: $date\n" .
+                "Mensaje: $message";
+
+        if (mail($to, $subject, $body, $headers)) {
+            header("Location: success.html");
+        } else {
+            header("Location: error.html");
+        }
+    }
+?>
